f0fef27cd4ccfd9de82247a3d7229a72486b300f
Ammar2521 <ammar.khalouf1021@gmail.com>
2025-09-21 06:56:18 -0700

    Update Program.cs
    helo

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, amanda min kalek!");
+            Console.WriteLine("Hello, alo!");
         }
     }
 }
